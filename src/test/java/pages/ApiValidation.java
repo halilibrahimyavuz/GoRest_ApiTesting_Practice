36b9002f233c0f24dd26db99a3e7db1724e9d83e
Halil Yavuz <halilibrahimyavuz07@gmail.com>
2023-10-04 17:28:22 +0300

    project_update

--- src/test/java/pages/ApiValidation.java
+++ src/test/java/pages/ApiValidation.java
@@ -1,69 +1,110 @@
 package pages;
 
-
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import utulities.ConfigurationReader;
+
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.given;
 
 public class ApiValidation {
-
     public Response response;
 
-    public Response postMethod(String name, String gender, String email, String status) {
-       // updateBody de ıcı dolu bır body gelecek
-        String requestBody = updateBody(name , gender ,email ,status);
-        Response response =given()
-                .headers("Authorization", "Bearer 104b4d1036cdb45868e51acab5843011b81ff25c5d3ac307d636317b880bc6dd" ,
-                    "Content-Type", "application/json" ,
-                            "Accept" , ContentType.JSON )
-                .body(requestBody)  // body i requestte gonderdık gelen cevap response de
-                .post();
-
-        return response;
-
-
-    }
-
     private String updateBody(String name, String gender, String email, String status) {
-        //post put pacht gıbı farklı methdoalrda kullanmak ıcın ortak method yazdım
-        //try catch yada throws kullanmamız lazım -
-        // try catch --> mesaj alma avantajımız var
-        //throws da bu methodu kulandıgım tüm methodlarda da throws kullanmam lazım kullanım zorlugu
-        //hata nasıl handle edılır -throws ve trycatch farkları nedır  ?
-
-        //{
-        //  "id": null,
-        //  "name": "replaceName",
-        //  "email": "replaceEmail",
-        //  "gender": "replaceGender",
-        //  "status": "replaceStatus"
-        //}
-        String body = null;
-
+        String body = "";
         try {
-
-            body=new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir")+ File.separator+"src/test/resources/data/userDetails.json")));
-          // body nın ıcındekılerı verıdgım parametreler ıle doldurup kendıme bır json data olusturuyor
-            body=body.replaceAll("replaceName", name);
-            body=body.replaceAll("replaceEmail", email);
-            body=body.replaceAll("replaceGender", gender);
-            body=body.replaceAll("replaceStatus", status);
+            body = new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir") + File.separator + "src/test/resources/data/userDetails.json")));
+            body = body.replaceAll("replaceName", name);
+            body = body.replaceAll("replaceEmail", email);
+            body = body.replaceAll("replaceGender", gender);
+            body = body.replaceAll("replaceStatus", status);
         } catch (IOException e) {
-            //sadece IOexception da esaj verır
             e.printStackTrace();
-            System.out.println(e.getMessage());
-            //hata mesajını yazdıermak ıcın kullandık
         }
-        //methodu kullanınca bana body gonderecek
         return body;
     }
 
+    public Response postMethod(String name, String gender, String email, String status) {
+        String requestBody = updateBody(name, gender, email, status);
+        Response response = given()
+                .headers("Authorization", "Bearer 7c1158095623c6486eae51ffd8f901971ac32060fe9dde5f0c94c14404817c0d",
+                        "Content-Type", "application/json", "Accept", ContentType.JSON)
+                .body(requestBody)
+                .post();
+        return response;
+    }
+
+    public Response putMethod(String name, String gender, String email, String status) {
+        String requestBody = updateBody(name, gender, email, status);
+        Response response = given()
+                .headers("Authorization", "Bearer 7c1158095623c6486eae51ffd8f901971ac32060fe9dde5f0c94c14404817c0d",
+                        "Content-Type", "application/json", "Accept", ContentType.JSON)
+                .body(requestBody)
+                .put();
+
+        return response;
+    }
 
 
+    private String updatePostBody(String postBody, String title) {
+        String body = "";
+        try {
+            body = new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir") + File.separator + "src/test/resources/data/postCreate.json")));
+            body = body.replaceAll("replaceBody", postBody);
+            body = body.replaceAll("replaceTitle", title);
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return body;
+    }
+
+    public Response postMethodCreate(String body, String title) {
+        String requestBody = updatePostBody(body, title);
+        Response response = given()
+                .headers("Authorization", "Bearer 7c1158095623c6486eae51ffd8f901971ac32060fe9dde5f0c94c14404817c0d",
+                        "Content-Type", "application/json", "Accept", ContentType.JSON)
+                .body(requestBody)
+                .post();
+
+        return response;
+    }
+
+    public String updatePostBody(String comment, String name, String email) {
+        String body = "";
+        try {
+            body = new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir") + File.separator + "src/test/resources/data/createComment.json")));
+            body = body.replaceAll("replaceBody", comment);
+            body = body.replaceAll("replaceName", name);
+            body = body.replaceAll("replaceEmail", email);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return body;
+    }
+    public Response postMethodComment(String comment, String name, String email) {
+        String requestBody = updatePostBody(comment, name,email);
+        Response response = given()
+                .headers("Authorization", "Bearer 7c1158095623c6486eae51ffd8f901971ac32060fe9dde5f0c94c14404817c0d",
+                        "Content-Type", "application/json", "Accept", ContentType.JSON)
+                .body(requestBody)
+                .post();
+
+        return response;
+    }
+
+    public Response deleteMethod() {
+        Response response=given()
+                .headers("Authorization","Bearer "+ ConfigurationReader.getProperty("bearerToken"),
+                        "Content-Type", "application/json", "Accept", ContentType.JSON)
+                .delete();
+        return response;
+    }
 }
--- src/test/resources/features/ApiTesting.feature
+++ src/test/resources/features/ApiTesting.feature
@@ -1,23 +1,72 @@
 Feature: Api testing for CRUID operators
 
-  Background: url
-    Given User given api url "https://gorest.co.in/"
+
+  Background:
+    Given user given api url "https://gorest.co.in"
 
   Scenario Outline: POST-Create a new user
-    Given User set api endpoint "public/v2/users"
-    And User create new user with request body "<Name>" , "<Gender>" ,"<Email>" ,"<Status>"
-    Then Validate the status code 201
-    And Validate the userId is not null
-    And Validate the user name is "<Name>"
-    And Validate the user gender is "<Gender>"
-    And Validate the user email is "<Email>"
-    And Validate the user status is "<Status>"
+    Given set api endpoint "public/v1/users"
+    And User creates new user with request body "<Name>","<Gender>","<Email>","<Status>"
+    Then validate the status code 201
+    And validate the userId is not null
+    And validate the user Name is "<Name>"
+    And validate the user Gender is "<Gender>"
+    And validate the user Email is "<Email>"
+    And validate the user Status is "<Status>"
+
+    Examples: User Details table
+      | Name       | Gender | Email                    | Status |
+      | Jo423423xx | male   | 423432hn2xxx60@gmail.com | active |
+
+
+  Scenario Outline: PUT-Update existing user data
+    Given set api endpoint "public/v1/users/""<userId>"
+    And Update the user with request body "<Name>","<Gender>","<Email>","<Status>"
+    Then validate the status code 200
+    And validate the user Name is "<Name>"
+    And validate the user Gender is "<Gender>"
+    And validate the user Email is "<Email>"
+    And validate the user Status is "<Status>"
+
+    Examples: User Details table
+      | Name  | Gender | Email              | Status | userId  |
+      | Joxx3 | male   | johwdwd6@gmail.com | active | 5292759 |
+
+
+  Scenario Outline: Create a post and comment
+    Given user sets "<EndpointPost>" post
+    And create a post with given userId and create one "<Body>" and "<Title>"
+    When user sets "<EndpointComment>" post and create one "<Comment>" using "<userId>", "<name>", "<email>", "<comment body>"
+    Then verify that comment created "<name>" "<email>"
+
+    Examples:
+      | EndpointPost                   | EndpointComment                   | userId | Body              | Title                  | Comment       | name    | email             | comment body         |
+      | public/v1/users/<userId>/posts | public/v1/posts/<userId>/comments | 13954  | Body post message | this is my first title | first comment | My Name | asdasda@gmail.com | This is comment body |
+
+
+  Scenario: DELETE-Delete user
+    Given user set endpoint "public-api/users/" for "5292759" userid
+    And use DELETE request to delete specific user
+    Then validate the response status code is 204
+    Then try to get the user data and validate response code is 404
+    Then validate with response message "Resource not found"
 
 
 
-    Examples:    #düzenleme için ctrl+alt+L
 
-      | Name  | Gender | Email           | Status |
-      | Sare Betul2  | female | sare222@gmail.com  | active |
 
+
+
+    ####################################################
+  # Alternate scenario
+
+ #Scenario: POST-Create a new user input
+ #  Given set api endpoint "public/v1/users"
+ #  And User creates new user with request body "John Doe 8","male","john26@gmail.comn","active"
+ #  Then validate the status code 201
+ #  And validate the userId is not null
+ #  And validate the user Name is "John Doe 8"
+ #  And validate the user Gender is "male"
+ #  And validate the user Email is "john26@gmail.comn"
+ #  And validate the user Status is "active"
 
